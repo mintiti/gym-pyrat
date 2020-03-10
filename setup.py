dfc453f5328fc997ec7081a8bf6befc95bd5d28e
Minh Tri <minhtri3007@hotmail.com>
2020-03-11 00:30:00 +0100

    Auto stash before merge of "master" and "origin/dev/tree-cleaning"

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='gym-pyrat',
+setup(name='gym_pyrat',
       version='0.0.1',
       install_requires=['gym','pygame', 'numpy'],
       author = 'Minh Tri Truong'
