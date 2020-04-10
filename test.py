a8ab5b787a1ed80ae0e7c555e1a7534c8c26e6fc
Minh Tri <ngoc-minh-tri.truong@imt-atlantique.net>
2020-04-10 02:12:28 +0200

    Simple test file

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
 from pyrat_env.envs import Pyrat_Env
 from pyrat_env.wrappers import AlphaZero, MatricizePositions, FinalReward
 import gym
+import numpy as np
+import time
+
 if __name__ == '__main__':
     env = gym.make("PyRatEnv-v0")
-
     env = AlphaZero(env)
+    env.reset()
+    unwr = env.unwrapped
+    done = False
+    while not done:
+        obs, rew, done, _ = env.step(np.random.randint(4, size=(2,)))
+        print(obs.shape)
+        print(obs[5])
+        print(obs[6])
+        print(rew, done)
 
-    print(isinstance(env,MatricizePositions))
-    print(isinstance(env,FinalReward))
+        time.sleep(0.2)
