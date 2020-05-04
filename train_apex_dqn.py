3fa1d71800c17fd8781a61a62eeda28cbd3fd06c
Minh Tri <ngoc-minh-tri.truong@imt-atlantique.net>
2020-05-04 19:39:37 +0200

    Testing parameters

--- train_apex_dqn.py
+++ train_apex_dqn.py
@@ -4,11 +4,15 @@ from pyrat_env.envs import PyratMultiAgent
 import ray
 from ray import tune
 from ray.rllib.agents.dqn.apex import APEX_DEFAULT_CONFIG, ApexTrainer
+
+
 APEX_DEFAULT_CONFIG['num_workers'] = 4
+APEX_DEFAULT_CONFIG["num_envs_per_worker"] = 16
 APEX_DEFAULT_CONFIG['use_pytorch'] = False
 APEX_DEFAULT_CONFIG['env'] = PyratMultiAgent
+APEX_DEFAULT_CONFIG['env_config'] = {'flatten' : True, 'target_density' : 0, 'symmetry' : False}
+APEX_DEFAULT_CONFIG["buffer_size"] = 100000
+APEX_DEFAULT_CONFIG["target_network_update_freq"] = 20000
 if __name__ == '__main__':
-    env = PyratMultiAgent()
-    obs = env.reset()
     ray.init()
-    tune.run(ApexTrainer, config = APEX_DEFAULT_CONFIG)
+    tune.run("APEX", config = APEX_DEFAULT_CONFIG)
