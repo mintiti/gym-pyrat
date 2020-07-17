b1e629b58780d1e8c44e0645b38f6de462f16d7f
iPown <minhtri3007@hotmail.com>
2020-07-17 15:54:10 +0200

    Starting refactoring in preparation for future work

--- pyrat_env/wrappers.py
+++ pyrat_env/wrappers.py
@@ -1,57 +1,19 @@
-from gym.core import ObservationWrapper, RewardWrapper
+from gym.core import ObservationWrapper, RewardWrapper, ActionWrapper
 import gym.spaces as spaces
 import numpy as np
 from gym.spaces import Box, Dict, Discrete, Tuple, MultiBinary, MultiDiscrete
 
 
-def flatdim2(space):
-    if isinstance(space, Box):
-        return int(np.prod(space.shape))
-    elif isinstance(space, Discrete):
-        return 1
-    elif isinstance(space, Tuple):
-        return int(sum([flatdim2(s) for s in space.spaces]))
-    elif isinstance(space, Dict):
-        return int(sum([flatdim2(s) for s in space.spaces.values()]))
-    elif isinstance(space, MultiBinary):
-        return int(space.n)
-    elif isinstance(space, MultiDiscrete):
-        return int(np.prod(space.shape))
-    else:
-        raise NotImplementedError
 
 
-def flatten2(space, x):
-    if isinstance(space, Box):
-        return np.asarray(x, dtype=np.float16).flatten()
-    elif isinstance(space, Discrete):
-        return [x]
-    elif isinstance(space, Tuple):
-        return np.concatenate([flatten2(s, x_part) for x_part, s in zip(x, space.spaces)])
-    elif isinstance(space, Dict):
-        return np.concatenate([flatten2(s, x[key]) for key, s in space.spaces.items()])
-    elif isinstance(space, MultiBinary):
-        return np.asarray(x).flatten()
-    elif isinstance(space, MultiDiscrete):
-        return np.asarray(x).flatten()
-    else:
-        raise NotImplementedError
+# TODO : Wrapper to play vs greedy
+class VsGreedy(ActionWrapper):
+    pass
 
 
-class FlattenObservationMatrices(ObservationWrapper):
+class AlphaZeroMatricizePositions(ObservationWrapper):
     def __init__(self, env):
-        super(FlattenObservationMatrices, self).__init__(env)
-
-        flatdim = flatdim2(env.observation_space)
-        self.observation_space = Box(low=-float('inf'), high=float('inf'), shape=(flatdim,), dtype=np.float16)
-
-    def observation(self, observation):
-        return flatten2(self.env.observation_space, observation)
-
-
-class MatricizePositions(ObservationWrapper):
-    def __init__(self, env):
-        super(MatricizePositions, self).__init__(env)
+        super(AlphaZeroMatricizePositions, self).__init__(env)
 
         obs_dim = (9, 21, 15)
         self.observation_space = Box(low=-float('inf'), high=float('inf'), shape=obs_dim, dtype=np.float16)
@@ -80,7 +42,8 @@ class MatricizePositions(ObservationWrapper):
 
         return np.array(ret, dtype= np.float16)
 
-
+class MatricizeObservation(ObservationWrapper):
+    def __init__(self,env,):
 class FinalReward(RewardWrapper):
     def __init__(self, env):
         super(FinalReward, self).__init__(env)
@@ -101,4 +64,4 @@ class FinalReward(RewardWrapper):
 
 
 def AlphaZero(env):
-    return FinalReward(MatricizePositions(env))
+    return FinalReward(AlphaZeroMatricizePositions(env))
